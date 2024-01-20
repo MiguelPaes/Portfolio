8f2ce4870c4965b2aaf830f988eb8b1a75723f8e
João Miguel Paes <100474687+MiguelPaes@users.noreply.github.com>
2024-01-20 17:03:17 +0000

    Iniciando Section e Implementaçao do Carrossel de tecnologias

--- Header/header.js
+++ Header/header.js
@@ -1,4 +1,8 @@
 window.addEventListener('scroll', function(){
-    let header = document.getElementById('#header')
-    header.classList.toggle('rolagem' , window.scrollY > 0)// o toggle diz o seguinte: se a class existir, remova, se nao existir adicone.
+    let header = document.querySelector('#header')
+    header.classList.toggle('rolagem' , window.scrollY > 100)// o toggle diz o seguinte: se a class existir, remova, se nao existir adicone.
 })
+
+
+
+
